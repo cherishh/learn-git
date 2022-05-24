46807a9f1265396bf4dd165169dc6ecf38cb40f1
wangzhongxi <wangzhongxi@meituan.com>
2022-05-24 21:10:20 +0800

    feat: f1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import App from "./App";
+import App from './App';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
+
+const Test = (
+  <div>
+    <div>f1</div>
+  </div>
+);
 
 root.render(
   <StrictMode>
     <App />
+    <Test />
   </StrictMode>
 );
