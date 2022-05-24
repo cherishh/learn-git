8dcbb3d614cfc4ddfd063d8e420b894ea2388f61
wangzhongxi <wangzhongxi@meituan.com>
2022-05-24 21:07:56 +0800

    main c1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import "./styles.css";
+import './styles.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className='App'>
+      <h1>C1</h1>
     </div>
   );
 }
